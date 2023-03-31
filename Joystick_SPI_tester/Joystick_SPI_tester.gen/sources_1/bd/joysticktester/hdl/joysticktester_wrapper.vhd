572df904eccd7c197cb7281f1deeda2758cb3c9a
Leo Stexo <info@stexo.eu>
2023-03-31 17:37:15 +0200

    dev project for the joystick communication

---
+++ Joystick_SPI_tester/Joystick_SPI_tester.gen/sources_1/bd/joysticktester/hdl/joysticktester_wrapper.vhd
@@ -0,0 +1,110 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
+--Date        : Fri Mar 31 16:13:08 2023
+--Host        : unbound-phoenix running 64-bit unknown
+--Command     : generate_target joysticktester_wrapper.bd
+--Design      : joysticktester_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity joysticktester_wrapper is
+  port (
+    SPI_M_0_io0_io : inout STD_LOGIC;
+    SPI_M_0_io1_io : inout STD_LOGIC;
+    SPI_M_0_sck_io : inout STD_LOGIC;
+    SPI_M_0_ss_io : inout STD_LOGIC;
+    reset : in STD_LOGIC;
+    sys_clock : in STD_LOGIC
+  );
+end joysticktester_wrapper;
+
+architecture STRUCTURE of joysticktester_wrapper is
+  component joysticktester is
+  port (
+    sys_clock : in STD_LOGIC;
+    reset : in STD_LOGIC;
+    SPI_M_0_sck_t : out STD_LOGIC;
+    SPI_M_0_io1_o : out STD_LOGIC;
+    SPI_M_0_ss_t : out STD_LOGIC;
+    SPI_M_0_io0_o : out STD_LOGIC;
+    SPI_M_0_sck_i : in STD_LOGIC;
+    SPI_M_0_ss_o : out STD_LOGIC;
+    SPI_M_0_io0_t : out STD_LOGIC;
+    SPI_M_0_io1_t : out STD_LOGIC;
+    SPI_M_0_sck_o : out STD_LOGIC;
+    SPI_M_0_ss_i : in STD_LOGIC;
+    SPI_M_0_io1_i : in STD_LOGIC;
+    SPI_M_0_io0_i : in STD_LOGIC
+  );
+  end component joysticktester;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal SPI_M_0_io0_i : STD_LOGIC;
+  signal SPI_M_0_io0_o : STD_LOGIC;
+  signal SPI_M_0_io0_t : STD_LOGIC;
+  signal SPI_M_0_io1_i : STD_LOGIC;
+  signal SPI_M_0_io1_o : STD_LOGIC;
+  signal SPI_M_0_io1_t : STD_LOGIC;
+  signal SPI_M_0_sck_i : STD_LOGIC;
+  signal SPI_M_0_sck_o : STD_LOGIC;
+  signal SPI_M_0_sck_t : STD_LOGIC;
+  signal SPI_M_0_ss_i : STD_LOGIC;
+  signal SPI_M_0_ss_o : STD_LOGIC;
+  signal SPI_M_0_ss_t : STD_LOGIC;
+begin
+SPI_M_0_io0_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_io0_o,
+      IO => SPI_M_0_io0_io,
+      O => SPI_M_0_io0_i,
+      T => SPI_M_0_io0_t
+    );
+SPI_M_0_io1_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_io1_o,
+      IO => SPI_M_0_io1_io,
+      O => SPI_M_0_io1_i,
+      T => SPI_M_0_io1_t
+    );
+SPI_M_0_sck_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_sck_o,
+      IO => SPI_M_0_sck_io,
+      O => SPI_M_0_sck_i,
+      T => SPI_M_0_sck_t
+    );
+SPI_M_0_ss_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_ss_o,
+      IO => SPI_M_0_ss_io,
+      O => SPI_M_0_ss_i,
+      T => SPI_M_0_ss_t
+    );
+joysticktester_i: component joysticktester
+     port map (
+      SPI_M_0_io0_i => SPI_M_0_io0_i,
+      SPI_M_0_io0_o => SPI_M_0_io0_o,
+      SPI_M_0_io0_t => SPI_M_0_io0_t,
+      SPI_M_0_io1_i => SPI_M_0_io1_i,
+      SPI_M_0_io1_o => SPI_M_0_io1_o,
+      SPI_M_0_io1_t => SPI_M_0_io1_t,
+      SPI_M_0_sck_i => SPI_M_0_sck_i,
+      SPI_M_0_sck_o => SPI_M_0_sck_o,
+      SPI_M_0_sck_t => SPI_M_0_sck_t,
+      SPI_M_0_ss_i => SPI_M_0_ss_i,
+      SPI_M_0_ss_o => SPI_M_0_ss_o,
+      SPI_M_0_ss_t => SPI_M_0_ss_t,
+      reset => reset,
+      sys_clock => sys_clock
+    );
+end STRUCTURE;
---
+++ Joystick_SPI_tester/Joystick_SPI_tester.srcs/sim_1/new/joystick_tb.vhd
@@ -0,0 +1,133 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity joystick_tb is
+end joystick_tb;
+
+architecture Behavioral of joystick_tb is
+
+begin
+
+    ------------------------ Constant Declaration -------------------------
+    -- Constant For Test Bench (TB) --
+    constant    RESET_ON    :   STD_LOGIC := '0';
+
+    constant    CLK_PERIOD 	: time := 10 ns;
+    constant    RESET_WND 	: time := 2 ms;
+    ----------------------------------
+    ------ Constant For DUT v2 ------
+    constant    DUT2_NUM_OF_SWS  :   NATURAL    := 16;
+    constant    DUT2_NUM_OF_LEDS   :   POSITIVE   := 16;
+    constant    DUT2_CLK_PERIOD_NS	   :   POSITIVE   := 10;
+    constant    DUT2_MIN_KITT_CAR_STEP_MS    :   POSITIVE    := 2;
+    constant    DUT2_TAIL_LENGTH    :   INTEGER := 4;
+    ----------------------------------
+    ----------------------------------------------------------------------
+
+
+    ----------------- Device Under Test (DUT) Declaration ----------------
+
+    ------------ DUT v2 --------------
+    COMPONENT  digilent_jstk2_0 
+        generic (
+            DELAY_US		: integer := 25;    -- Delay (in us) between two packets
+            CLKFREQ		 	: integer := 100_000_000;  -- Frequency of the aclk signal (in Hz)
+            SPI_SCLKFREQ 	: integer := 66_666 -- Frequency of the SPI SCLK clock signal (in Hz)
+        );
+        Port (
+            aclk 			: in  STD_LOGIC;
+            aresetn			: in  STD_LOGIC;
+
+            -- Data going TO the SPI IP-Core (and so, to the JSTK2 module)
+            m_axis_tvalid	: out STD_LOGIC;
+            m_axis_tdata	: out STD_LOGIC_VECTOR(7 downto 0);
+            m_axis_tready	: in STD_LOGIC;
+
+            -- Data coming FROM the SPI IP-Core (and so, from the JSTK2 module)
+            -- There is no tready signal, so you must be always ready to accept and use the incoming data, or it will be lost!
+            s_axis_tvalid	: in STD_LOGIC;
+            s_axis_tdata	: in STD_LOGIC_VECTOR(7 downto 0);
+
+            -- Joystick and button values read from the module
+            jstk_x			: out std_logic_vector(9 downto 0);
+            jstk_y			: out std_logic_vector(9 downto 0);
+            btn_jstk		: out std_logic;
+            btn_trigger		: out std_logic;
+
+            -- LED color to send to the module
+            led_r			: in std_logic_vector(7 downto 0);
+            led_g			: in std_logic_vector(7 downto 0);
+            led_b			: in std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    ----------------------------------------------------------------------
+
+    ------------------------- Signal Declaration -------------------------
+    ---------- Reset/Clock ----------
+    signal  reset   :   STD_LOGIC := RESET_ON;
+    signal  clk     :   STD_LOGIC := '1';
+    ---------------------------------
+
+    -------- KittCar ----------------
+    signal  dut2_sw   :   STD_LOGIC_VECTOR(DUT2_NUM_OF_SWS-1 downto 0) := (Others => '0');
+    signal  dut2_leds  :   STD_LOGIC_VECTOR(DUT2_NUM_OF_LEDS-1 downto 0);
+    ---------------------------------
+    ----------------------------------------------------------------------
+
+begin
+
+    ------------------- Device Under Test (DUT) Wrapper ------------------
+
+    ------------ DUT v2 --------------
+    dut2_KittCarPWM   :   KittCarPWM
+
+        Generic Map(
+            CLK_PERIOD_NS => DUT2_CLK_PERIOD_NS,		
+		    MIN_KITT_CAR_STEP_MS =>	DUT2_MIN_KITT_CAR_STEP_MS,	
+		    NUM_OF_SWS =>   DUT2_NUM_OF_SWS,
+		    NUM_OF_LEDS =>	DUT2_NUM_OF_LEDS,
+            TAIL_LENGTH => DUT2_TAIL_LENGTH
+        )
+        Port Map(
+            ---------- Reset/Clock ----------
+            reset  => reset,
+            clk    => clk,
+            ---------------------------------
+            ------------- Data --------------
+            sw => dut2_sw,
+		    leds => dut2_leds
+            ---------------------------------
+        );
+    -----------------------------------------------------------------------
+
+    -------------------------- Signals Generation -------------------------
+
+    ------ TB Clk Generation -------
+    clk	<= not clk after CLK_PERIOD/2;
+    ---------------------------------
+
+    ----- TB Reset Generation ------
+    reset_wave : process
+    begin
+
+        reset <= RESET_ON;
+        wait for RESET_WND;
+
+        reset <= not RESET_ON;
+        wait;
+
+    end process;
+    ---------------------------------
+
+    -- TB din pattern Generation ---
+    dut2_din_pattern : process
+    begin
+        --not needed to wait for reset window since sw remain always at 0
+        dut2_sw <= (Others => '0');
+        wait;
+
+    end process;
+    ---------------------------------
+    -----------------------------------------------------------------------
+end Behavioral;
