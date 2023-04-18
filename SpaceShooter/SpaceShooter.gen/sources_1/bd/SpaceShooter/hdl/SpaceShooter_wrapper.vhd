78e480dc85561041fec8e93ff462fb45a006a279
Leo Stexo <info@stexo.eu>
2023-04-18 12:32:31 +0200

    cleanup

--- SpaceShooter/SpaceShooter.gen/sources_1/bd/SpaceShooter/hdl/SpaceShooter_wrapper.vhd
+++ SpaceShooter/SpaceShooter.gen/sources_1/bd/SpaceShooter/hdl/SpaceShooter_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
---Date        : Wed Mar 29 16:09:57 2023
+--Date        : Wed Apr 12 18:22:44 2023
 --Host        : unbound-phoenix running 64-bit unknown
 --Command     : generate_target SpaceShooter_wrapper.bd
 --Design      : SpaceShooter_wrapper
@@ -13,19 +13,104 @@ library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
 entity SpaceShooter_wrapper is
   port (
-    sys_clock : in STD_LOGIC
+    SPI_M_0_io0_io : inout STD_LOGIC;
+    SPI_M_0_io1_io : inout STD_LOGIC;
+    SPI_M_0_sck_io : inout STD_LOGIC;
+    SPI_M_0_ss_io : inout STD_LOGIC;
+    reset : in STD_LOGIC;
+    sys_clock : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
   );
 end SpaceShooter_wrapper;
 
 architecture STRUCTURE of SpaceShooter_wrapper is
   component SpaceShooter is
   port (
-    sys_clock : in STD_LOGIC
+    sys_clock : in STD_LOGIC;
+    reset : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    SPI_M_0_sck_t : out STD_LOGIC;
+    SPI_M_0_io1_o : out STD_LOGIC;
+    SPI_M_0_ss_t : out STD_LOGIC;
+    SPI_M_0_io0_o : out STD_LOGIC;
+    SPI_M_0_sck_i : in STD_LOGIC;
+    SPI_M_0_ss_o : out STD_LOGIC;
+    SPI_M_0_io0_t : out STD_LOGIC;
+    SPI_M_0_io1_t : out STD_LOGIC;
+    SPI_M_0_sck_o : out STD_LOGIC;
+    SPI_M_0_ss_i : in STD_LOGIC;
+    SPI_M_0_io1_i : in STD_LOGIC;
+    SPI_M_0_io0_i : in STD_LOGIC
   );
   end component SpaceShooter;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal SPI_M_0_io0_i : STD_LOGIC;
+  signal SPI_M_0_io0_o : STD_LOGIC;
+  signal SPI_M_0_io0_t : STD_LOGIC;
+  signal SPI_M_0_io1_i : STD_LOGIC;
+  signal SPI_M_0_io1_o : STD_LOGIC;
+  signal SPI_M_0_io1_t : STD_LOGIC;
+  signal SPI_M_0_sck_i : STD_LOGIC;
+  signal SPI_M_0_sck_o : STD_LOGIC;
+  signal SPI_M_0_sck_t : STD_LOGIC;
+  signal SPI_M_0_ss_i : STD_LOGIC;
+  signal SPI_M_0_ss_o : STD_LOGIC;
+  signal SPI_M_0_ss_t : STD_LOGIC;
 begin
+SPI_M_0_io0_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_io0_o,
+      IO => SPI_M_0_io0_io,
+      O => SPI_M_0_io0_i,
+      T => SPI_M_0_io0_t
+    );
+SPI_M_0_io1_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_io1_o,
+      IO => SPI_M_0_io1_io,
+      O => SPI_M_0_io1_i,
+      T => SPI_M_0_io1_t
+    );
+SPI_M_0_sck_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_sck_o,
+      IO => SPI_M_0_sck_io,
+      O => SPI_M_0_sck_i,
+      T => SPI_M_0_sck_t
+    );
+SPI_M_0_ss_iobuf: component IOBUF
+     port map (
+      I => SPI_M_0_ss_o,
+      IO => SPI_M_0_ss_io,
+      O => SPI_M_0_ss_i,
+      T => SPI_M_0_ss_t
+    );
 SpaceShooter_i: component SpaceShooter
      port map (
-      sys_clock => sys_clock
+      SPI_M_0_io0_i => SPI_M_0_io0_i,
+      SPI_M_0_io0_o => SPI_M_0_io0_o,
+      SPI_M_0_io0_t => SPI_M_0_io0_t,
+      SPI_M_0_io1_i => SPI_M_0_io1_i,
+      SPI_M_0_io1_o => SPI_M_0_io1_o,
+      SPI_M_0_io1_t => SPI_M_0_io1_t,
+      SPI_M_0_sck_i => SPI_M_0_sck_i,
+      SPI_M_0_sck_o => SPI_M_0_sck_o,
+      SPI_M_0_sck_t => SPI_M_0_sck_t,
+      SPI_M_0_ss_i => SPI_M_0_ss_i,
+      SPI_M_0_ss_o => SPI_M_0_ss_o,
+      SPI_M_0_ss_t => SPI_M_0_ss_t,
+      reset => reset,
+      sys_clock => sys_clock,
+      usb_uart_rxd => usb_uart_rxd,
+      usb_uart_txd => usb_uart_txd
     );
 end STRUCTURE;
