8fa47e87307851d8ab877e7f163bc4456e61ad86
Luca452 <daidoneluca98@gmail.com>
2023-05-05 16:40:52 +0200

    ROMPOTUTTO_PT3

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/Debouncer.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/Debouncer.vhd
@@ -1,43 +1,62 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05.05.2023 15:42:59
--- Design Name: 
--- Module Name: Debouncer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Debouncer is
---  Port ( );
+    generic (
+		DEBOUNCE_MS		: positive := 10;
+		CLOCK_FREQ		: positive := 100_000_000
+	);
+	Port (
+		-- Clock and reset
+		clk				: in std_logic;
+		reset			: in std_logic;
+
+		-- I/Os
+		input_signal	: in std_logic;
+		debounced		: out std_logic
+	);
 end Debouncer;
 
 architecture Behavioral of Debouncer is
+    
+    constant COUNTER_HIGH	: integer := CLOCK_FREQ / 1000 * DEBOUNCE_MS - 1;
+
+	signal debounced_int	: std_logic;
+	signal counter			: integer range 0 to COUNTER_HIGH;
 
 begin
+    -- To be able to read the value of input_debounced (which is an "out" port)
+	-- we have to declare a signal and assign that to the external port.
+	debounced	<= debounced_int;
 
+	process(clk, reset)
+	begin
+		if reset = '1' then
+
+			debounced_int	<= input_signal;
+			counter			<= 0;
+
+		elsif rising_edge(clk) then
+
+			if counter = 0 then
+
+				-- Keep sampling input_signal.
+				debounced_int	<= input_signal;
+
+				-- If input_signal has changed from the last sample, start
+				-- the timer.
+				if debounced_int /= input_signal then
+					counter		<= COUNTER_HIGH;
+				end if;
+
+			else
+				-- When the timer is running, no change is recorded, so the
+				-- output stays constant.
+				counter	 <= counter - 1;
+			end if;
+
+		end if;
+	end process;
 
 end Behavioral;
