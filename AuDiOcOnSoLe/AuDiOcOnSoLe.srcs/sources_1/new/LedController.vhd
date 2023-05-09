c1599e04ad8ab37c1f630dd0b4cd99be0c7489c3
Luca452 <daidoneluca98@gmail.com>
2023-05-09 17:27:08 +0200

    Revert "Merge branch 'main' of https://github.com/Leostx/DESD_LABS"
    This reverts commit 97454640f15d7859ab02ca02ec6dd963ad99438d, reversing
    changes made to 6f0a58b53f00979f954a5eb132a4b4d21156621c.

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/LedController.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/LedController.vhd
@@ -32,43 +32,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity LedController is
-	Port ( 
-		--inputs 
-		mute_enable 	: in STD_LOGIC;
-		filter_enable 	: in STD_LOGIC;
-		--outputs
-		led_r		: out std_logic_vector(7 downto 0);
-		led_g		: out std_logic_vector(7 downto 0);
-		led_b		: out std_logic_vector(7 downto 0)
-	);
+--  Port ( );
 end LedController;
 
 architecture Behavioral of LedController is
-    	signal led_r_reg : std_logic_vector(7 downto 0);
-    	signal led_g_reg : std_logic_vector(7 downto 0);
-    	signal led_b_reg : std_logic_vector(7 downto 0);
-    
+
 begin
-	--if muted led is red
-	if mute_enable = '1' then
-		led_r_reg <= X"FF";
-		led_b_reg <= X"0";
-		led_g_reg <= X"0";
-	--if the filter is enabled led is blue
-	elsif filter_enable = '1' then
-		led_r_reg <= X"0";
-		led_b_reg <= X"FF";
-		led_g_reg <= X"0";
-	--if no effect is applied it is green
-	else	
-		led_r_reg <= X"0";
-		led_b_reg <= X"0";
-		led_g_reg <= X"FF";
-	end if;
-	
-	led_r <= led_r_reg;
-        led_g <= led_g_reg;
-        led_b <= led_b_reg;
 
 
 end Behavioral;
