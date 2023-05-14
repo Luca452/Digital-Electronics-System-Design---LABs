85c66d24231f8b17a3669ea87699c9529af7455d
Leo Stexo <info@stexo.eu>
2023-05-14 15:37:16 +0200

    Volume Controller brought forward

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/VolumeController.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/VolumeController.vhd
@@ -35,10 +35,14 @@ architecture Behavioral of VolumeController is
 	signal tx_state      : tx_state_type;
     
     -- signal to hold the absolute positive value of the volume, adjusted to have a maximum of 512
-    signal pos_vol       : std_logic_vector(volume'HIGH downto 0);
+    signal abs_vol       : std_logic_vector(volume'HIGH downto 0);
+
+    -- signal to hold the absolute value of the data
+    signal abs_data      : std_logic_vector(s_axis_tdata'HIGH downto 0);
     
-    -- signal to hold the logarithmic volume level
-    signal vol_N         : natural range 0 to VOLUME_BITS;
+    -- signal to multiply and clip the data
+    signal data_reg      : std_logic_vector(31 downto 0);
+
     
     -- signals that hold the volume data
     signal data_reg_l    : std_logic_vector(23 downto 0);
@@ -46,41 +50,61 @@ architecture Behavioral of VolumeController is
 
 begin
 
-    -- TODO take absolute value
-    pos_vol <= std_logic_vector(signed(unsigned(volume) - SUB_PADDING));
-    
-    --TODO think about making it sincronous, advantages? disadvantages?
-    -- priority encoder, to get the MSB that is '1'
-    PRIO_ENC : process (pos_vol)
-    begin
-        vol_N <= 0;
-        for i in pos_vol'LOW to pos_vol'HIGH loop
-            if pos_vol(i) = '1' then
-                vol_N <= i;
-            end if;
-        end loop;
-    end process;
-    
+    abs_vol <= std_logic_vector(abs(signed(unsigned(volume) - SUB_PADDING)));
+    abs_data <= std_logic_vector(abs(signed(s_axis_tdata)));
     
     -- process to handle the axis receive part
     RX : process (aresetn, aclk)
+        variable vol_N : natural := 0;
     begin
         if aresetn = '0' then
         -- TODO should the reset get handled?
         -- reset is not handled
         elsif rising_edge(aclk) then
 
-            if s_axis_tvalid = '1' then
-                -- TODO multiply the data, aka increase the volume
+            -- priority encoder, to get the MSB that is '1'
+            for i in abs_vol'LOW to abs_vol'HIGH loop
+                if abs_vol(i) = '1' then
+                    vol_N := i;
+                end if;
+            end loop;
 
-                if s_axis_tlast = '0' then
-                    data_reg_l <= s_axis_tdata;
-                    s_axis_tready <= '1';
+            if s_axis_tvalid = '1' then
+                -- TODO check if its working. Check the registers where things get passed to. there could be problems, since it is sync
+
+                -- muliply the incoming data only if the volume knob is moved
+                if vol_N > 0 then
+
+                    -- check if the N (power of 2 to multiply) MSB have a 1 in them.
+                    -- don't consider the MSB, since it is the sign bit
+                    -- if they have, the signal has to saturate. So put all the data, except from the sign bit 
+                    -- otherwise shift by N
+                    -- the sign bit is for shure = '0' since we've taken only the absolute value
+                    if unsigned(abs_data((abs_data'HIGH-1) downto (abs_data'HIGH - vol_N))) /= 0 then
+                        abs_data <= (abs_data'HIGH => '0', Others => '1');
+                    else
+                        abs_data <= std_logic_vector(unsigned(abs_data) sll vol_N);
+                    end if;
+
+                    -- convert the absolut value back to a signed value, depending on the position of the joystick
+                    if unsigned(volume) > 512 then
+                        data_reg <= std_logic_vector(signed(abs_data));
+                    else 
+                        data_reg <= std_logic_vector(-signed(abs_data));
+                    end if;
+
                 else
-                    data_reg_r <= s_axis_tdata;
-                    s_axis_tready <= '1';
+                    data_reg <= s_axis_tdata;
                 end if;
 
+                -- assign the data to the internal register depending on the fact if it is data for the right or for the left channel. Discriminating by the tlast signal
+                if s_axis_tlast = '0' then
+                    data_reg_l <= data_reg;
+                    s_axis_tready <= '1';
+                else
+                    data_reg_r <= data_reg;
+                    s_axis_tready <= '1';
+                end if;
             end if;
 
         end if;    
