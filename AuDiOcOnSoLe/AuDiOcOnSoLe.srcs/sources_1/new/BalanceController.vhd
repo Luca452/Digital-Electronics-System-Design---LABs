bc8886344af91a6dffe666e0e2d32e6817c26e6d
riky192 <riccardo.ronchi@cloudlift.online>
2023-05-15 23:11:17 +0200

    boh spero vada bene, domani riguardo

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/BalanceController.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/BalanceController.vhd
@@ -33,51 +33,44 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity BalanceController is
     Generic(
-        BALANCE_BITS     : integer := 9
+        BALANCE_BITS     : integer := 10
+        AXIS_TDATA_WIDTH : integer	:= 24;
+        N_BALANCE         : positive := 6
     );
     Port (         
         aclk            :   in   STD_LOGIC;   
         aresetn         :   in   STD_LOGIC;  
     
-        m_axis_tvalid   : out STD_LOGIC;
-        m_axis_tdata    : out STD_LOGIC_VECTOR(23 downto 0);
-        m_axis_tready   : in  STD_LOGIC;
-        m_axis_tlast    : out STD_LOGIC;
+        ------------AXI4-Stream--slave-------------
+        S_AXIS_TVALID	:   in   STD_LOGIC;
+        -- TDATA is the primary payload that is used to provide the data that is passing across the interface from the master
+        S_AXIS_TDATA	:   in 	 STD_LOGIC_VECTOR(AXIS_TDATA_WIDTH-1 DOWNTO 0);
+        -- TREADY indicates that the slave can accept a transfer in the current cycle
+        S_AXIS_TREADY	:   out  STD_LOGIC := '1';
+        -- AXI4Stream tLAST to distinguish between left and right channel
+        S_AXIS_TLAST	:   in   STD_LOGIC;
+        --------------------------------------------
 
-        s_axis_tvalid   : in  STD_LOGIC;
-        s_axis_tdata    : in  STD_LOGIC_VECTOR(23 downto 0);
-        s_axis_tready   : out STD_LOGIC;
-        s_axis_tlast    : in  STD_LOGIC;
+        ------------AXI4-Stream--master-------------
+        M_AXIS_TREADY	:   in 	 STD_LOGIC;
+        -- Data in
+        M_AXIS_TDATA	:   out  STD_LOGIC_VECTOR(AXIS_TDATA_WIDTH-1 DOWNTO 0) := (Others =>'0');
+        -- AXI4Stream tLAST to distinguish between left and right channel
+        M_AXIS_TLAST	:   out  STD_LOGIC;
+        -- Data is in valid
+        M_AXIS_TVALID	:   out  STD_LOGIC := '0';
+        --------------------------------------------
 
-        balance          : in STD_LOGIC_VECTOR(BALANCE_BITS downto 0)
+        balance          : in STD_LOGIC_VECTOR(BALANCE_BITS - 1 downto 0)
     );
 end BalanceController;
 
 architecture Behavioral of BalanceController is
-    -- maximum single side value of the balance 
-    -- to substract from the total value to align the volume controll around zero
-    constant SUB_PADDING : integer := 2**BALANCE_BITS;
-
-    -- states for the TX state machine
-	type tx_state_type is (IDLE, SEND_L, SEND_R);
-	signal tx_state      : tx_state_type;
-    
-    -- signal to hold the absolute positive value of the pan, adjusted to have a maximum of 512
-    signal pos_bal       : std_logic_vector(balance'HIGH downto 0);
-    
-    -- signal to hold the logarithmic balance level
-    signal bal_N         : natural range 0 to BALANCE_BITS;
-    
-    -- signals that hold the volume data
-    signal data_reg_l    : std_logic_vector(23 downto 0);
-    signal data_reg_r    : std_logic_vector(23 downto 0);
-
-begin
-    pos_bal <= std_logic_vector(abs(signed(unsigned(balance) - SUB_PADDING)));
-
-    --Need to implement the priority encoder right
-    --it will output bal_N which is 
-    
+    -- we take the 10-N bits as a counter to know in which interval we are,
+    -- but MSB bit is used to discriminate between pan left/right so 10-N-1 
+    constant  max_balance : positive := 2**(BALANCE_BITS - N_BALANCE - 1)-1;
+    signal    bal_N   : integer  := 0;
+begin    
     -- process to handle the axis receive part
     RX : process (aresetn, aclk)
     begin
@@ -91,33 +84,26 @@ begin
                 if s_axis_tlast = '0' then  --left channel
                     data_reg_l <= s_axis_tdata;
                     s_axis_tready <= '1';
+
+                    --if balance > 544 (joystick moved right I hope) then left channel has to be attenuated 
+                    if(balance > '544') then
+                        bal_N <=  max_balance - to_integer(unsigned(balance(VOLUME_BITS-2 downto N_BALANCE)));
+                        data_reg_l <= data_reg_l srl bal_N;
+                    end if;
+                    
                 else                        --right channel
                     data_reg_r <= s_axis_tdata;
                     s_axis_tready <= '1';
-                    new_data <= '1';
+
+                    --if balance < 480 (joystick moved left I hope) then right channel has to be attenuated 
+                    if(balance < '480') then
+                        bal_N <=  to_integer(unsigned(balance(VOLUME_BITS-2 downto N_BALANCE)));
+                        data_reg_r <= data_reg_r srl bal_N;
+                    end if;
                 end if;
-
             end if;
-
         end if;    
     end process;
-
-
-    dataprocess: process (aresetn, aclk)
-    begin
-        if aresetn = '0' then
-            -- TODO should the reset get handled?
-            -- reset is not handled
-        elsif rising_edge(aclk) then
-            if new_data = '1' then
-            end if;
-        end if;
-    end process;
-
-
-
-
-
 
 -- process to handle the axis send part
     TX : process (aresetn, aclk)
@@ -151,9 +137,7 @@ begin
                     if m_axis_tready = '1' then
                         tx_state <= IDLE;
                     end if;                        
-
             end case;
-            
         end if;
     end process;
 
