3236f995a23ca5a276f754fc2eafb90c4b14c183
Luca452 <daidoneluca98@gmail.com>
2023-05-16 09:49:37 +0200

    Andato avanti con il volume, non funziona ancora

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/imports/new/Volume.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/imports/new/Volume.vhd
@@ -45,50 +45,76 @@ architecture Behavioral of Volume is
 
     signal    abs_data : std_logic_vector(AXIS_TDATA_WIDTH-1 DOWNTO 0);
 
+    -- signals that hold the volume data
+    signal data_reg_R    : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0);
+
+    signal data_L    : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0') ;
+    signal data_R    : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0');
+
+    -- states for the TX state machine
+	type tx_state_type is (IDLE, SEND_L, SEND_R);
+	signal tx_state      : tx_state_type;
+
 
 begin
 
-    abs_data <= std_logic_vector(abs(signed(S_AXIS_TDATA)));
-    
-    process(aresetn, aclk)
-    begin
-        if(aresetn = '0') then
+    --process(aresetn, aclk)
+    --begin
+    --    if(aresetn = '0') then
             -- da fare
-        elsif rising_edge(aclk) then
+    --    elsif rising_edge(aclk) then
             -- if MSB of volume is '0' then divide, else multiply
-            if(volume(volume'high) = '0') then
-                vol_N <=  max_vol - to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)));
-                abs_data <= std_logic_vector(unsigned(abs_data) srl vol_N);
-            else
-                vol_N <=  to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)));
-                -- CLIPPING 
-                if unsigned(abs_data((abs_data'HIGH-1) downto (abs_data'HIGH - vol_N))) /= 0 then
-                    abs_data <= (abs_data'HIGH => '0', Others => '1');
-                else
-                    abs_data <= std_logic_vector(unsigned(abs_data) sll vol_N);
-                end if;
-            end if;
-        end if;
-    end process;
+    --       if(volume(volume'high) = '0' and volume(N_VOLUME-1) = '1') then
+    --            vol_N <=  max_vol - to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)));
+    --        elsif(volume(volume'high) = '1' and volume(N_VOLUME-1) = '1') then
+    --           vol_N <=  to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)));
+    --        end if;
+    --    end if;
+    --end process;
+
+    abs_data <= std_logic_vector(abs(signed(S_AXIS_TDATA)));
+
+    vol_N <= (max_vol - to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)))) when (volume(volume'left) = '0' and volume(N_VOLUME-1) = '1') 
+        else to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)))             when (volume(volume'left) = '1' and volume(N_VOLUME-1) = '1'); 
+
 
     -- process to handle the axis receive part
     RX : process (aresetn, aclk)
         variable vol_N : natural := 0;
     begin
         if aresetn = '0' then
-        -- TODO should the reset get handled?
-        -- reset is not handled
+            -- da fare
         elsif rising_edge(aclk) then
 
             if s_axis_tvalid = '1' then
-                -- NON SO COSA STO FACENDO AAAAAAAAAAAAAAAAAA
                 -- assign the data to the internal register depending on the fact if it is data for the right or for the left channel. Discriminating by the tlast signal
+
                 if s_axis_tlast = '0' then
-                    data_reg_l <= data_reg;
-                    s_axis_tready <= '1';
+
+                    if(volume(volume'left) = '0') then
+                        data_L <= std_logic_vector(shift_right(unsigned(abs_data), vol_N));
+                    elsif (volume(volume'left) = '1') then
+                        -- CLIPPING 
+                        if unsigned(abs_data((abs_data'left) downto (abs_data'left - vol_N))) /= 0 then
+                            data_L <= (abs_data'left => '0', Others => '1');
+                        else
+                            data_L <= std_logic_vector(shift_left(unsigned(abs_data), vol_N));
+                        end if;
+                    end if;
+
                 else
-                    data_reg_r <= data_reg;
-                    s_axis_tready <= '1';
+
+                    if(volume(volume'high) = '0') then
+                        data_reg_R <= std_logic_vector(unsigned(abs_data) srl vol_N);
+                    else
+                        -- CLIPPING 
+                        if unsigned(abs_data((abs_data'left) downto (abs_data'left - vol_N))) /= 0 then
+                            data_reg_R <= (data_reg_R'left => '0', Others => '1');
+                        else
+                            data_reg_R <= std_logic_vector(unsigned(abs_data) sll vol_N);
+                        end if;
+                    end if;
+                    
                 end if;
             end if;
 
@@ -109,22 +135,24 @@ begin
 
                 when IDLE =>
                     m_axis_tvalid <= '0';
-                    m_axis_tlast <= '0';
+                    if(s_axis_tlast = '0') then
+                        m_axis_tlast <= '0';
+                        tx_state <= SEND_L;
+                    else 
+                        m_axis_tlast <= '1';
+                        tx_state <= SEND_R;
+                    end if;
 
                 when SEND_L =>
-                    m_axis_tdata <= data_reg_l;
-                    m_axis_tlast <= '0';
                     m_axis_tvalid <= '1';
-
+                    m_axis_tdata <= data_L;
                     if m_axis_tready = '1' then
-                        tx_state <= SEND_R;
+                        tx_state <= IDLE;
                     end if;
                         
                 when SEND_R =>
-                    m_axis_tdata <= data_reg_r;
-                    m_axis_tlast <= '1';
                     m_axis_tvalid <= '1';
-
+                    m_axis_tdata <= data_reg_R;
                     if m_axis_tready = '1' then
                         tx_state <= IDLE;
                     end if;                        
