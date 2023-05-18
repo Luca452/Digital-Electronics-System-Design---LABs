cbb9b4991a003ba05431fe1e460552f0c5e79acb
Leo Stexo <info@stexo.eu>
2023-05-18 16:24:59 +0200

    Some Volume fixes - simulation done in my head. Does work 100%

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/VolumeController.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/VolumeController.vhd
@@ -42,19 +42,14 @@ architecture Behavioral of VolumeController is
     -- but 1 bit is used to discriminate between divide/multiply so 10-N-1 
     -- max_vol is the number of intervals we've got
     -- vol_N is the interval we are in, based on the volume input
-    -- constant  max_vol : positive := 2**(VOLUME_BITS - N_VOLUME - 1)-1; -- TODO Lucas original version, can be deleted if new one works
     constant  max_vol : positive := 2**(VOLUME_BITS - N_VOLUME - 1);
     signal    vol_N   : integer  := 0;
 
     -- abs data used to define when clipping is needed
     signal    abs_data : std_logic_vector(AXIS_TDATA_WIDTH-1 DOWNTO 0);
 
-    -- signals that hold the volume data
-    -- signal data_L    : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0); -- TODO is not needed, as we put the data directly on the line
-    signal data_R    : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0);
-
     -- states for the state machine
-	type state_type is (RECEIVE, SEND_L, SEND_R);
+	type state_type is (RECEIVE, SEND);
 	signal state     : state_type;
 
 begin
@@ -71,12 +66,6 @@ begin
     vol_N <= (max_vol - to_integer( unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)) + unsigned(volume(N_VOLUME-1 downto N_VOLUME-1)) ))  when (volume(volume'left) = '0') 
                     else to_integer( unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)) + unsigned(volume(N_VOLUME-1 downto N_VOLUME-1)) )  when (volume(volume'left) = '1'); 
 
-    -- this only works, if we hit the value in the transitioning reagion, since for example for N=6 the values between 576 and 607 have this N-1 bit = 0
-    -- TODO can't assign based on volume(N_VOLUME-1) = '1' since for example the values between 576 and 607 have this bit = 0
-    -- vol_N <= (max_vol - to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)))) when (volume(volume'left) = '0' and volume(N_VOLUME-1) = '1') 
-    --    else to_integer(unsigned(volume(VOLUME_BITS-2 downto N_VOLUME)))             when (volume(volume'left) = '1' and volume(N_VOLUME-1) = '1'); 
-    -- TODO Lucas original version, can be deleted if new one works
-    
     -- process to handle the data
     -- everything is handled by states in a single process, to stay in sync between incoming and outgoing data
     FSM : process (aresetn, aclk)
@@ -86,7 +75,6 @@ begin
         -- reset is not handled
         elsif rising_edge(aclk) then
 
-
             case state is
 
                 when RECEIVE =>
@@ -94,81 +82,61 @@ begin
                     -- process the data on the data line only if it's valid
                     if s_axis_tvalid = '1' then
 
+                        -- if the MSB of volume is 1, the value is >= 512, so the volume has the volume has to be increased,
+                        -- thus the data to be multiplied, otherwise divided
+                        -- the multiplicatio and division is implementet with a bitshift in the according direction by vol_N bits
+                        if(volume(volume'high) = '0') then
+
+                            -- to save a clock cycle we put the data directly on the axis line
+                            m_axis_tdata <= std_logic_vector(shift_right(signed(S_AXIS_TDATA),vol_N));
+
+                        elsif (volume(volume'high) = '1') then
+
+                            -- if the left most N bits of the absolute value of the data are different than 0,
+                            -- it means that a shift would bring the data outside of range and thus the signal has to be clipped at it's maximum possible value
+                            if unsigned(abs_data((abs_data'high) downto (abs_data'high- vol_N))) /= 0 then
+
+                                -- if the highest bit of the incoming data is 0, the value is positive and so has to be the clipping,
+                                -- otherwise the data is negative and the clipping has to occur at the minimum
+                                if s_axis_tdata(s_axis_tdata'high) = '0' then
+                                    m_axis_tdata <= (m_axis_tdata'high => '0', Others => '1');
+                                elsif s_axis_tdata(s_axis_tdata'high) = '1' then
+                                    m_axis_tdata <= (m_axis_tdata'high => '1', Others => '0');
+                                end if;
+
+                            else
+                                m_axis_tdata <= std_logic_vector(shift_left(signed(S_AXIS_TDATA), vol_N));
+                            end if;
+
+                        end if;
+
+
                         -- discriminate between left and right channel, if tlast = '1' -> right channel, otherwise left 
                         if s_axis_tlast = '0' then
                             
-                            -- if the MSB of volume is 1, the value is >= 512, so the volume has the volume has to be increased,
-                            -- thus the data to be multiplied, otherwise divided
-                            -- the multiplicatio and division is implementet with a bitshift in the according direction by vol_N bits
-                            if(volume(volume'left) = '0') then
-                                -- to save a clock cycle we put the data directly on the axis line
-                                m_axis_tdata <= std_logic_vector(shift_right(signed(S_AXIS_TDATA),vol_N));
-                            elsif (volume(volume'left) = '1') then
-
-                                -- if the left most N bits of the absolute value of the data are different than 0,
-                                -- it means that a shift would bring the data outside of range and thus the signal has to be clipped at it's maximum possible value
-                                if unsigned(abs_data((abs_data'left) downto (abs_data'left - vol_N))) /= 0 then
-                                    m_axis_tdata <= (m_axis_tdata'left => '0', Others => '1');
-                                else
-                                    m_axis_tdata <= std_logic_vector(shift_left(signed(S_AXIS_TDATA), vol_N));
-                                end if;
-
-                            end if;
-
-                        else
-
-                            -- same reasoning as before but for the right channel
-                            if(volume(volume'high) = '0') then
-                                data_R <= std_logic_vector(shift_right(signed(S_AXIS_TDATA),vol_N));
-                            elsif (volume(volume'left) = '1') then
-
-                                -- CLIPPING 
-                                if unsigned(abs_data((abs_data'left) downto (abs_data'left - vol_N))) /= 0 then
-                                    data_R <= (data_R'left => '0', Others => '1');
-                                else
-                                    data_R <= std_logic_vector(shift_left(signed(S_AXIS_TDATA), vol_N));
-                                end if;
-
-                            end if;
-
-                            -- when the elaboration of the right channel is done, the data can be send in the next clock phase
-                            
-                            -- don't receive any more data
-                            s_axis_tready <= '0';
-                            -- put the data for the the left channel on the line
-                            -- m_axis_tdata <= data_L; -- TODO not needed as we put the data directly on the line 
                             -- since it is the left channel,  put tlast to 0
                             m_axis_tlast <= '0';
-                            -- set the data on the line to valid
-                            m_axis_tvalid <= '1';
-                            -- set the next state to be the sending of the left data
-                            state <= SEND_L;
+
+                        else
+                            
+                            -- since it is the right channel,  put tlast to 0
+                            m_axis_tlast <= '1';
                             
                         end if;
-                    end if;
 
-
-                when SEND_L =>
-
-                    -- if the receiver is ready to receive more data go on
-                    if m_axis_tready = '1' then
-
-                        -- put the data for the the right channel on the line
-                        m_axis_tdata <= data_R;
-                        -- since it is the right channel, put tlast to 1
-                        m_axis_tlast <= '1';
+                        -- don't receive any more data
+                        s_axis_tready <= '0';
                         -- set the data on the line to valid
                         m_axis_tvalid <= '1';
-                        -- set the next state to be the sending of the right data
-                        state <= SEND_R;
+                        -- set the next state to be the sending of the data
+                        state <= SEND;
 
                     end if;
                         
-                when SEND_R =>
+                when SEND =>
 
-                    -- if the receiver is ready go on
+                    -- if the receiver is ready go on to receiving again 
                     -- wait for the receiver to be ready, as we are going to invalidate the data on the line, to be ready to receive 
-                    -- TODO since the data is still on the line, is it necessary to wait for the tready? Probably yes, as we are going to invalidate the line
                     if m_axis_tready = '1' then
 
                         -- invalidate the data on the line
