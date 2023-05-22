5677cf5a5f50ae309aca8887a837c2359cffb529
Luca452 <daidoneluca98@gmail.com>
2023-05-22 23:59:29 +0200

    HO COMMENTATO TUTTO DA SOLO MA TANTO NESSUNO LEGGERA' MAI
    SIETE DEGLI SCANSAFATICHE INCREDIBILI, scemo chi legge bush did 9 11 non siamo mai andati sulla luna, dovete a me il vostro voto perdigiorno

--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/BalanceController.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/BalanceController.vhd
@@ -4,25 +4,39 @@ library IEEE;
 	use IEEE.NUMERIC_STD.ALL;
 ------------------------------------
 ---------------------------------------CODE EXPLANATION----------------------------------------------
+-- The volume is divided into equal ranges, where each range consists of a power of 2 number of elements.
+-- This allows us to exploit the variation of the binary number between range limits to develop an efficient
+-- circuit for coding the volume range.
+-- For example, if we choose N=6, it means that a volume range must consist of 2^N=64 elements. By considering
+-- only the first (10-N) bits of the JSTK value, we can determine when the threshold of a range is exceeded.
+-- When 64 is added to the JSTK value in binary, it corresponds to adding 1 to the number represented by the 
+-- (10-N) bits considered. These bits effectively function as a counter, indicating the exact volume level.
+-- Additionally, the first bit indicates whether the halfway point of the dynamics has been crossed,
+-- signifying whether the JSTK is on the LEFT or on the RIGHT, meaning that the other channel must be
+-- decreased in order to implement a panning effect. 
 
 -- FSM EXPLANATION, same as other modules, can avoid reading it if already did :)
--- This VHDL code implements a 4-state finite state machine to handle data receiving/processing/transmitting in a serial manner.
--- The machine waits for data of both channels and performs appropriate processing before transmitting it.
--- The choice of a 4-state finite state machine is justified by the nature of the input data, which arrives in a sequential manner,
--- since left and right channels data is known to arrive one after the other in a single packet.
+-- This VHDL code implements a 4-state finite state machine to handle the sequential receiving, processing,
+-- and transmitting of data in a serial manner. The machine waits for data from both channels and performs
+-- appropriate processing before transmitting it. The choice of a 4-state finite state machine is justified
+-- by the nature of the input data, which arrives sequentially. Specifically, the left and right channel data
+-- is known to arrive one after the other in a single packet. 
 -- By employing a serial approach, we ensure the safety and simplicity of data handling.
--- Additionally, employing serial data transmission simplifies the design and implementation of the system
--- by eliminating the need for complex parallel data handling mechanisms.
--- This is possible considering that the frequency of the AXI protocol clock is significantly higher than that of the audio signals.
--- Overall, this design choice optimizes data handling efficiency while ensuring reliability and ease of implementation.
+-- Moreover, utilizing serial data transmission simplifies the design and implementation of the system by
+-- eliminating the need for complex parallel data handling mechanisms. This is feasible because the frequency
+-- of the AXI protocol clock is significantly higher than that of the audio signals. In summary, this design
+-- choice optimizes data handling efficiency while ensuring reliability and ease of implementation.
 -----------------------------------------------------------------------------------------------------
 entity BalanceController is
     Generic(
         AXIS_TDATA_WIDTH : positive	:= 24;
         BALANCE_BITS     : positive := 10;
-        N_BALANCE        : positive range 1 to 9 := 6
+        -- ideally N_BALANCE highest value should be constrained by BALANCE_BITS,
+        -- we rely on the fact that BALANCE_BITS will always remain constant
+        N_BALANCE        : positive range 1 to 8 := 6
     );
-    Port (         
+    Port (
+        -- Clock and reset          
         aclk            :   in   STD_LOGIC;   
         aresetn         :   in   STD_LOGIC;  
     
@@ -46,28 +60,35 @@ entity BalanceController is
         M_AXIS_TVALID	:   out  STD_LOGIC := '0';
         --------------------------------------------
 
+        -- Input from JSTK module
         balance          : in STD_LOGIC_VECTOR(BALANCE_BITS - 1 downto 0)
     );
 end BalanceController;
 
 architecture Behavioral of BalanceController is
+
+    --------------------------------------SIGNALS & CONSTANTS----------------------------------------
     -- we take the 10-N bits as a counter to know in which interval we are,
-    -- but MSB bit is used to discriminate between pan left/right so 10-N-1 
+    -- but 1 bit is used to discriminate between L or R of the JSTK so 10-N-1 
+    -- max_balance is the number of intervals we've got
+    -- bal_N is the interval we are in, based on the volume input
     constant    max_balance     : positive := 2**(BALANCE_BITS - N_BALANCE - 1);
     signal      bal_N           : integer  := 0;
+
+    -- signal to save the data for the right channel
     signal      DATA_R          : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0');
     
-    -- states for the TX state machine
+    -- states for the state machine
 	type state_type is (RECEIVE_L, RECEIVE_R, SEND_L, SEND_R);
 	signal state     : state_type := RECEIVE_L;
+    -------------------------------------------------------------------------------------------------
 
 begin    
 
-    
+    -- FSM process
     FSM : process (aresetn, aclk)
     begin
         if aresetn = '0' then
-        -- TODO should the reset get handled?
         -- reset is not handled
         elsif rising_edge(aclk) then
 
@@ -79,11 +100,11 @@ begin
                     if S_AXIS_TLAST = '0' and S_AXIS_TVALID = '1' then
                         -- if joystick is to the right then attenuate left channel and put it in the bus otherwise write it directly on the bus
                         if (balance(balance'left) = '1') then
-                            -- calculate the volume range we must be in
-                            -- namely the number of bits to shift
-                            -- on every transitioning value the bit volume(N_VOLUME-1) is 1
-                            -- then take the three bits above, namely volume(VOLUME_BITS-2 downto N_VOLUME) which represent a unsigned in the range from 0 to max_vol, so we've got our N
-                            -- if we have to decrease the volume subtract this from the maximum possible N
+                            -- calculate the volume range we must be in, namely the number of bits to shift
+                            -- on every transitioning value. The bit balance(N_BALANCE-1) is 1
+                            -- then take the three bits above, namely balance(BALANCE_BITS-2 downto N_BALANCE) which represent an unsigned
+                            -- in the range from 0 to max_balance, so we've got our bal_N
+                            -- if we have to decrease the volume subtract this from the maximum possible bal_N
                            bal_N <= (to_integer(unsigned(balance(BALANCE_BITS-2 downto N_BALANCE))) 
                            + to_integer(unsigned(balance(N_BALANCE-1 DOWNTO N_BALANCE-1)))); 
 
@@ -94,12 +115,13 @@ begin
 
                         -- ready to receive the next data
                         S_AXIS_TREADY <= '1';
-
+                        -- go to receive right state
                         state <= RECEIVE_R; 
                     end if;
 
                 when RECEIVE_R => 
 
+                    -- process the data only if TVALID = '1', TLAST discriminates between channels
                     if S_AXIS_TLAST = '1' and S_AXIS_TVALID = '1' then
                         -- if joystick is to the left then attenuate right channel and put it in a register otherwise write it directly in the register
                         if (balance(balance'left) = '0') then
--- AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/MobileMean.vhd
+++ AuDiOcOnSoLe/AuDiOcOnSoLe.srcs/sources_1/new/MobileMean.vhd
@@ -6,24 +6,28 @@ library IEEE;
 ------------------------------------
 
 ---------------------------------------CODE EXPLANATION----------------------------------------------
--- The mobile mean is implemented by storing the oncoming data in a shift register made of
--- AVERAGING_WINDOW_SIZE elements, such elements are also added to a SUM_TOTAL register
--- as soon as they arrive, while the last element of the shif register is
--- always subtracted from the SUM_TOTAL register. In this way each clock cycle only a sum and a 
--- subtraction are performed. Each clock edge the values in the shif register are shifted by one position.
--- In order to implement the power of 2 division, just the 24 MSB of the SUM_TOTAL register are taken
--- and sent on the data bus.
+-- The mobile mean is implemented using a shift register consisting of AVERAGING_WINDOW_SIZE elements.
+-- The incoming data is stored in this register, and it is simultaneously added to a SUM_TOTAL register
+-- as soon as it arrives. At each clock cycle, the last element of the shift register is subtracted from
+-- the SUM_TOTAL register. This process ensures that only one addition and one subtraction operation are
+-- performed per clock cycle. Additionally, during each clock edge, the values in the shift register are 
+-- shifted by one position, discarding the oldest element and making room for the new data point.
+-- To implement power of 2 division, only the 24 most significant bits (MSB) of the SUM_TOTAL register are
+-- taken and sent on the data bus. This allows for efficient division by powers of 2 without the need for 
+-- additional operations. By not using a FIFO structure with pointers, two BRAM (Block RAM) modules that
+-- would have otherwise been instantiated are saved.
 
 -- FSM EXPLANATION, same as other modules, can avoid reading it if already did :)
--- This VHDL code implements a 4-state finite state machine to handle data receiving/processing/transmitting in a serial manner.
--- The machine waits for data of both channels and performs appropriate processing before transmitting it.
--- The choice of a 4-state finite state machine is justified by the nature of the input data, which arrives in a sequential manner,
--- since left and right channels data is known to arrive one after the other in a single packet.
+-- This VHDL code implements a 4-state finite state machine to handle the sequential receiving, processing,
+-- and transmitting of data in a serial manner. The machine waits for data from both channels and performs
+-- appropriate processing before transmitting it. The choice of a 4-state finite state machine is justified
+-- by the nature of the input data, which arrives sequentially. Specifically, the left and right channel data
+-- is known to arrive one after the other in a single packet. 
 -- By employing a serial approach, we ensure the safety and simplicity of data handling.
--- Additionally, employing serial data transmission simplifies the design and implementation of the system
--- by eliminating the need for complex parallel data handling mechanisms.
--- This is possible considering that the frequency of the AXI protocol clock is significantly higher than that of the audio signals.
--- Overall, this design choice optimizes data handling efficiency while ensuring reliability and ease of implementation.
+-- Moreover, utilizing serial data transmission simplifies the design and implementation of the system by
+-- eliminating the need for complex parallel data handling mechanisms. This is feasible because the frequency
+-- of the AXI protocol clock is significantly higher than that of the audio signals. In summary, this design
+-- choice optimizes data handling efficiency while ensuring reliability and ease of implementation.
 -----------------------------------------------------------------------------------------------------
 
 entity MobileMean is
@@ -68,6 +72,7 @@ architecture Behavioral of MobileMean is
 
     --------------------------------------SIGNALS & CONSTANTS----------------------------------------
     -- Length of the sum register, which stores the sum of all the window elements 
+    -- AVERAGING_WINDOW_SIZE elements of AXIS_TDATA_WIDTH length are added, necessary number of bits is calculated
     constant SUM_TOTAL_LENGTH : positive := (AXIS_TDATA_WIDTH + positive(ceil(log2(real(AVERAGING_WINDOW_SIZE)))));
 
     -- Registers that store the sum
@@ -80,7 +85,7 @@ architecture Behavioral of MobileMean is
     signal S_AXIS_TREADY_int : std_logic := '0';
     signal M_AXIS_TDATA_int : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0');
 
-    -- Register for the result of the R channel
+    -- Register for the result of the R channel average
     signal DATA_R : std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0) := (others => '0');
 
     -- Shift registers for the L and R channels
@@ -99,12 +104,12 @@ begin
     FSM : process (aresetn, aclk)
     begin
         if aresetn = '0' then
-        
+            -- reset is not handled
         elsif rising_edge(aclk) then
 
             -- this mux for the filter_en can't be performed in DATAFLOW,
-            -- since after implementation the total negative slack is non null due to longer signals path for clock >=180MHz
-            -- a register is needed since I'm not able to perform path optimization :(
+            -- since after implementation the total negative slack for clock >=180MHz is non null due to longer signals path.
+            -- registering is needed since I'm not able to perform path optimization
             case filter_enable is 
                 when '0' => 
                             M_AXIS_TDATA <= S_AXIS_TDATA; 
@@ -122,13 +127,13 @@ begin
 
                 when RECEIVE_L =>
 
-                    -- process the data only if TVALID = '1', TLAST discriminates between channels 
+                    -- process the data only if TVALID = '1', and filter enabled, TLAST discriminates between channels 
                     if S_AXIS_TLAST = '0' and filter_enable = '1' and S_AXIS_TVALID = '1' then
 
                         ------------------------------------AVG VALUE CALCULATION-----------------------------------------
-                        -- fill the first position of the memory with the oncoming data
+                        -- fill the first position of the shift register with the oncoming data ad shift other elements
                         mem_L <= S_AXIS_TDATA & mem_L(0 to AVERAGING_WINDOW_SIZE-2);
-                        -- oncoming data is always summed to the total, last element of the memory is subtracted
+                        -- incoming data is always summed to the total, last element of the shift register is subtracted
                         SUM_TOTAL_L <= SUM_TOTAL_L + signed(std_logic_vector(resize(signed(S_AXIS_TDATA),SUM_TOTAL_LENGTH))) 
                                                    - signed(std_logic_vector(resize(signed(mem_L(AVERAGING_WINDOW_SIZE-1)),SUM_TOTAL_LENGTH)));
                         --------------------------------------------------------------------------------------------------
@@ -145,10 +150,11 @@ begin
 
                 when RECEIVE_R => 
 
+                    -- process the data only if TVALID = '1', and filter enabled, TLAST discriminates between channels
                     if S_AXIS_TLAST = '1' and filter_enable = '1' and S_AXIS_TVALID = '1' then
 
                         ------------------------------------AVG VALUE CALCULATION-----------------------------------------
-                        -- same calculation as before but for the other channel
+                        -- same calculations as before but for the other channel
                         mem_R <= S_AXIS_TDATA & mem_R(0 to AVERAGING_WINDOW_SIZE-2);
                         SUM_TOTAL_R <= SUM_TOTAL_R + signed(std_logic_vector(resize(signed(S_AXIS_TDATA),SUM_TOTAL_LENGTH))) 
                                                    - signed(std_logic_vector(resize(signed(mem_R(AVERAGING_WINDOW_SIZE-1)),SUM_TOTAL_LENGTH)));                            
@@ -160,9 +166,9 @@ begin
 
                         -- don't receive any more data
                         S_AXIS_TREADY_int <= '0';
-                        -- writing on the L channel
+                        -- writing the L channel element on the bus
                         M_AXIS_TLAST_int <= '0';
-                        -- ready to send data, set master valid to '1'
+                        -- data is ready on the bus, set master valid to '1'
                         M_AXIS_TVALID_int <= '1';
                         -- set the next state to be the sending of the left data
                         state <= SEND_L;     
@@ -172,9 +178,9 @@ begin
 
                     -- if slave is ready, means it read the data on the bus, put new data on the bus and change state
                     if M_AXIS_TREADY = '1' then
-                        -- writing on the R channel
+                        -- writing the R channel element on the bus
                         M_AXIS_TLAST_int <= '1';
-                        -- when the elaboration of the right channel is done, the data can be send in the next clock phase
+                        -- put R channel data on the bus
                         M_AXIS_TDATA_int <= DATA_R;
                         -- set the next state to be the receiving of data
                         state <= SEND_R;
